2e957e848867e5d1e61cba971af19dc51afba69e
K Lars Lohn <twobraids@gmail.com>
2016-01-27 13:12:26 -0800

    Update reitercurse.py

--- reitercurse.py
+++ reitercurse.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+"""This project is an intellectual exercise, not a serious project for production use. 
+Maybe it will inspire someone to make a more serious version that accomplishes the 
+same thing with more more aplomb and efficiency. This implementation is likely full of 
+problems, can't-get-there-from-here situations, or other such troubles. 
+
+There is no warranty, no guarantee, and the author is absentee."""
+
 
 from mock import MagicMock
 from functools import wraps
